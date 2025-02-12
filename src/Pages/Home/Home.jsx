2e4fbc147bd38b36c1dff6674ebe2da3495d192f
mohamed2233996 <medomano771@gmail.com>
2025-02-12 18:59:17 +0200

    ✨feature: links in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <>
-    
-
-
-    </>
+    <div className='flex gap-6 text-center flex-col text-black font-bold'>
+    <a href="/Register">Register page</a>
+    <a href="/Login">Login page</a>
+    <a href="/ForgotPassword">Forgot Password page</a>
+    <a href="/ResetPassword">Reset Password page</a>
+    </div>
   )
 }
 
