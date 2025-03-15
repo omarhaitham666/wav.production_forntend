6038084697b0a4839d97a32b78a5fdd41251c57a
Mohamed Elsayed <mohamdeesayed@outlook.com>
2025-03-16 00:36:44 +0200

    disable un used import statment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import postcss from 'postcss'
-import CompressionPlugin from 'compression-webpack-plugin'
+// import postcss from 'postcss'
+// import CompressionPlugin from 'compression-webpack-plugin'
 
 
 
